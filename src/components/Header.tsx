8e545f6b2f6facba2b37ce71d48ac19a37679654
Shantanu Dutta <shantanudutta1994@gmail.com>
2021-06-19 19:19:39 +0530

    Added header.

---
+++ src/components/Header.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Header: React.FC = () => {
+    return (
+        <header>
+            <nav className="bg-white shadow">
+                <div className="container mx-2 sm:mx-4 mx-auto">
+                    <div className="flex items-center sm:items-stretch sm:justify-start h-16">
+                        <div className="flex-shrink-0 flex items-center">
+                            <h1 className="text-xl font-medium leading-relaxed inline-block md:ml-2 px-2 whitespace-nowrap text-blue">
+                                BMI Calculator
+                            </h1>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
